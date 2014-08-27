ba4bb8c65c579b6100fb9745139aaedd0cc3932e
Tom Janson <tom.janson@rwth-aachen.de>
2014-08-27 22:41:54 +0200

    example improved

--- example.js
+++ example.js
@@ -9,10 +9,11 @@ var rc = require('./piswitch');
 // Note that you'll need to export the pin with `gpio export [PIN] out`
 // when using 'sys' (non-root) mode, which is recommended.
 //   compare: http://wiringpi.com/reference/setup/
+//   pin chart: http://pi.gadgetoid.com/pinout/
 
 rc.setup({
     mode: 'sys', // alternative: change to gpio and use root
-    pulseLength: 330,
+    pulseLength: 330, // this works for me, but 350 is very common
     protocol: 1
 });
 
@@ -22,21 +23,23 @@ if (process.argv.length < 3) {
   return;
 }
 
-var code = process.argv[2];
-console.log("Code: " + code);
+var code    = process.argv[2];
+var type    = process.argv[3];
+var offFlag = process.argv[4];
+var offMsg  = '';
+var off;
 
-var type, off;
-
-if (typeof process.argv[3] !== undefined) {
-  type = process.argv[3]
-  console.log("Type declared as: " + type);
+if (typeof type === 'undefined') {
+  console.log("[INFO] Implicitly declared as binary.");
+  type = 'binary';
 }
 
-if (typeof process.argv[4] !== undefined && process.argv[4] === 'off') {
+if (typeof offFlag !== 'undefined' && offFlag === 'off') {
   off = true;
-  console.log("Sending OFF ...");
-} else {
-  console.log("Sending ON ...");
+  offMsg = '-OFF';
+} else if (type !== 'binary' && type !== 'tristate') {
+  offMsg = '-ON';
 }
 
+console.log("[SEND] " + code + offMsg + " (type declared as: '" + type + "')");
 rc.send(code, type, off);
